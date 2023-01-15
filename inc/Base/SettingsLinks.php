e51a0d38f6c80c24f503b221b8e1d9e0368a72d8
Joener <joenermunch@gmail.com>
2023-01-15 18:26:09 -0300

    adding admin callbacks

--- inc/Base/SettingsLinks.php
+++ inc/Base/SettingsLinks.php
@@ -5,15 +5,13 @@
  */
 
 namespace Inc\Base;
+use Inc\Base\BaseController;
 
-class SettingsLinks {
 
-
-	function __construct() {
-	}
+class SettingsLinks extends BaseController {
 
 	public function register() {
-		add_filter( 'plugin_action_links_' . PLUGIN, array( $this, 'settings_link' ) );
+		add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
 	}
 
 	public function settings_link( $links ) {
@@ -22,3 +20,4 @@ class SettingsLinks {
 		return $links;
 	}
 }
+
--- lastfm-connector.php
+++ lastfm-connector.php
@@ -12,14 +12,32 @@ Text Domain: lastfm-connector
 
 defined( 'ABSPATH' ) or die( 'Not authorized.' );
 
+
+// Require Composer Autoload
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
-define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'PLUGIN', plugin_basename( __FILE__ ) );
+// // Define Constants
+// define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+// define( 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+// define( 'PLUGIN', plugin_basename( __FILE__ ) );
 
+// Runs on activation
+function activate_lastfm_connector() {
+	Inc\Base\Activate::activate();
+}
+register_activation_hook( __FILE__, 'activate_lastfm_connector' );
+
+
+// Runs on deactivation
+function deactivate_lastfm_connector() {
+	Inc\Base\Deactivate::deactivate();
+}
+register_deactivation_hook( __FILE__, 'deactivate_lastfm_connector' );
+
+
+// Initializes all of the core classes
 if ( class_exists( 'Inc\\Init' ) ) {
 	Inc\Init::register_services();
 }
