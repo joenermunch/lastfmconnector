cc3370ad4d9ea024511cf70f6bf964066fbc003c
Joener <joenermunch@gmail.com>
2023-01-16 21:25:43 -0300

    adding vendor build

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'joener/lastfm-connector',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a33fb4500287e5c9baca85c7e224454a6b469910',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'joener/lastfm-connector' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a33fb4500287e5c9baca85c7e224454a6b469910',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
