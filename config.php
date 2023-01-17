026cddeb9e5edc42af9f50343671b5cdc72479db
Joener <joenermunch@gmail.com>
2023-01-16 21:27:08 -0300

    removing old credentials

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-	'consumer_key'    => 'crhvTWomVFr9Oqs4P0e6Pza5l',
-	'consumer_secret' => 'zGAIeJXCRmvhJxUvHm3trw39mOGOGCYyhxF3kV4v2MzgvL058t',
-	'url_login'       => 'http://lastfm.local/wp-content/plugins/lastfm-connector/twitter_login.php',
-	'url_callback'    => 'http://lastfm.local/wp-content/plugins/lastfm-connector/twitter_callback.php',
-);
